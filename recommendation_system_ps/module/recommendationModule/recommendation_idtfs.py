875b9a8fb82773d2d55c4c3e5048ec23d024f10b
Ehor Hleoza <gleoza04@gmail.com>
2024-11-11 01:07:42 +0300

    add recommendation idtfs

---
+++ recommendation_system_ps/module/recommendationModule/recommendation_idtfs.py
@@ -0,0 +1,16 @@
+from dataclasses import dataclass
+
+from sc_kpm.sc_keynodes import Idtf
+
+
+@dataclass(frozen=True)
+class CommonIdentifiers:
+    NREL_USER_IDTF: Idtf = "nrel_user_idtf"
+    NREL_PLACE_IDTF: Idtf = "nrel_place_idtf"
+    RECOMMENDATION_MODEL: Idtf = "recommendation_model"
+    NREL_SERIALIZED: Idtf = "nrel_serialized"
+    ACTION_GET_RECOMMENDATION: Idtf = "action_get_recommendation"
+    ACTION_GET_CLASSIC_RECOMMENDATION: Idtf = "action_get_classic_recommendation"
+    ACTION_GET_CLASS_RECOMMENDATION: Idtf = "action_get_class_recommendation"
+    ACTION_GET_EXPERIMENTAL_RECOMMENDATION: Idtf = "action_get_experimental_recommendation"
+    
