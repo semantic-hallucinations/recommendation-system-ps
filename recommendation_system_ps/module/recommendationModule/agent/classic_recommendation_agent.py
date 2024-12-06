5f7d3a125590afcc46d9a80ccb835a15c5db55db
VTR(Konstantin Avrukevich) <avrukievich@gmail.com>
2024-12-06 11:20:07 +0300

    migrated to new kpm and client and added geo packages

--- recommendation_system_ps/module/recommendationModule/agent/classic_recommendation_agent.py
+++ recommendation_system_ps/module/recommendationModule/agent/classic_recommendation_agent.py
@@ -2,9 +2,6 @@ from sc_client.models import ScAddr
 from sc_kpm import ScResult, ScKeynodes
 from sc_kpm.sc_agent import ScAgentClassic
 import sc_kpm.utils as utils
-from sc_client.models import ScTemplate, ScConstruction
-from sc_client.client import create_elements
-from sc_client.constants import sc_types
 from sc_kpm.sc_sets import ScStructure
 from sc_kpm.identifiers import CommonIdentifiers
 from ..recommendation_idtfs import RecommendationIdentifiers
@@ -15,7 +12,7 @@ class ClassicRecommendationAgent(ScAgentClassic):
         super().__init__(RecommendationIdentifiers.ACTION_GET_CLASSIC_RECOMMENDATION)
 
     def on_event(self, event_element: ScAddr, event_edge: ScAddr, action_element: ScAddr) -> ScResult:
-        user = utils.get_element_by_role_relation(action_element, ScKeynodes.rrel_index(1))
+        user = utils.search_element_by_role_relation(action_element, ScKeynodes.rrel_index(1))
 
         if not user:
             utils.action_utils.finish_action_with_status(action_element, False)
@@ -35,9 +32,9 @@ class ClassicRecommendationAgent(ScAgentClassic):
             utils.action_utils.finish_action_with_status(action_element, False)
             return ScResult.NO
 
-        recommendation_action_answer = utils.action_utils.get_action_answer(recommendation_action_instance)
+        recommendation_action_answer = utils.action_utils.get_action_result(recommendation_action_instance)
         recommendation_action_answer = ScStructure(set_node=recommendation_action_answer)
 
-        utils.action_utils.create_action_answer(action_element, *recommendation_action_answer)
+        utils.action_utils.generate_action_result(action_element, *recommendation_action_answer)
         utils.action_utils.finish_action_with_status(action_element)
         return ScResult.OK
